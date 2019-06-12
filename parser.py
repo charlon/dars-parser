eb92ff1ce8e321bb5d2b485916bd9016e39b3786
Craig M. Stimmel <cstimmel@uw.edu>
2019-06-12 14:57:13 -0700

    Turn into something importable and open the actual file.

--- parser.py
+++ parser.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
 
 
-soup = BeautifulSoup('degrees/BS Aeronautical Astro Eng.html', 'html.parser')
+def main():
+    filepath = 'degrees/BS Aeronautical Astro Eng.html'
 
-print(soup.prettify())
+    with open(filepath) as fp:
+        myfile = fp.read()
+
+    soup = BeautifulSoup(myfile, 'html.parser')
+
+    print(soup.prettify())
+
+
+if __name__ == "__main__":
+    main()
