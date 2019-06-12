4420cf3c7db97dbeb6e00aa1f005313eff020557
Craig M. Stimmel <cstimmel@uw.edu>
2019-06-12 15:24:42 -0700

    return a soup object, rather than printing it.

--- parser.py
+++ parser.py
@@ -2,15 +2,17 @@ from bs4 import BeautifulSoup
 
 
 def parse(filepath):
-
+    """ Parses an html DARS audit.
+    """
     with open(filepath) as fp:
         myfile = fp.read()
 
     soup = BeautifulSoup(myfile, 'html.parser')
 
-    print(soup.prettify())
+    return soup
 
 
 if __name__ == "__main__":
     filepath = 'degrees/BS Aeronautical Astro Eng.html'
-    parse(filepath)
+    soup = parse(filepath)
+    print(soup.prettify())
